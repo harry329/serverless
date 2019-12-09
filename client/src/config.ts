650541471adcaad55ced7825f5456a8362578e03
harry329 <harry329@users.noreply.github.com>
2019-12-09 14:56:03 -0600

    Update serverless api endpoint

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'ibf19moeni'
+const apiId = 'o55we6juc8'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
